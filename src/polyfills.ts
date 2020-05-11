406b29819df928edce6c78b17480bb6b6a8e3cf9
João Vicente <joao.vicente@campus.ul.pt>
2020-05-11 17:20:39 +0100

    Added IE dependencies

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -46,7 +46,7 @@ import 'classlist.js';  // Run `npm install --save classlist.js`.
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
  * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
  **/
-//import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
@@ -73,3 +73,4 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+import 'core-js';
